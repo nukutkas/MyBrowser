fe4c807403749f1a225873faa94cfd22bc464101
Nikita Kochetov <nothsmple@mail.ru>
2020-08-25 20:47:40 +0300

    Initial Commit

---
+++ MyBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyBrowser
+//
+//  Created by Татьяна Кочетова on 25.08.2020.
+//  Copyright © 2020 Nikita Kochetov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
